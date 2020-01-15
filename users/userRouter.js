34716c7c227154a68ff69d17510e7b09358fa5ab
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-15 18:26:37 +0500

    Implemented user routes

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,38 +1,89 @@
 const express = require("express");
+const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", validateUser, async (req, res, next) => {
   // do your magic!
+  const newUserData = req.body;
+  try {
+    const newUser = await userDb.insert(newUserData);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/:id/posts", (req, res) => {
+router.post(
+  "/:id/posts",
+  validateUserId,
+  validatePost,
+  async (req, res, next) => {
+    // do your magic!
+    const user_id = req.params.id;
+    const text = req.body;
+    try {
+      const newPost = await postDb.insert({ text, user_id });
+      res.status(200).json(newPost);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get("/", async (req, res, next) => {
   // do your magic!
+  try {
+    const users = await userDb.get();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/", (req, res) => {
+router.get("/:id", validateUserId,  (req, res, next) => {
   // do your magic!
+    res.status(200).json(req.user);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id/posts", validateUserId, async (req, res, next) => {
   // do your magic!
+  const { id } = req.params;
+  try {
+    const posts = postDb.get();
+    res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/:id/posts", (req, res) => {
+router.delete("/:id", validateUserId, async (req, res, next) => {
   // do your magic!
+  const { id } = req.params;
+  try {
+    const numOfDeletedRecords = await userDb.remove(id);
+    res.status(200).json(numOfDeletedRecords);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.put("/:id", validateUserId, validateUser, async (req, res, next) => {
   // do your magic!
-});
-
-router.put("/:id", (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+  const userUpdateData = req.body;
+  try {
+    const updated = await userDb.update(id, userUpdateData);
+    res.status(200).json(updated);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //custom middleware
 
-export async function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   const { id } = req.params;
   try {
     const isValidId = await userDb.getById(id);
@@ -47,7 +98,7 @@ export async function validateUserId(req, res, next) {
   }
 }
 
-export function validateUser(req, res, next) {
+function validateUser(req, res, next) {
   if (req.body) {
     if (req.body.name) {
       next();
@@ -59,7 +110,7 @@ export function validateUser(req, res, next) {
   }
 }
 
-export function validatePost(req, res, next) {
+function validatePost(req, res, next) {
   if (req.body) {
     if (req.body.text) {
       next();
