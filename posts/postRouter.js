a849aaed61f54b375b77635977ca1bff67b78431
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-15 18:26:51 +0500

    Implemented  post routes

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,59 @@
-const express = require('express');
-
+const express = require("express");
+const postDb = require("./postDb");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res, next) => {
   // do your magic!
+  try {
+    const posts = await postDb.get();
+    res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validatePostId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validatePostId, async (req, res, next) => {
   // do your magic!
+  const { id } = req.params;
+  try {
+    await postDb.getById(id);
+    res.status(200).json(req.post);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validatePostId, async (req, res, next) => {
   // do your magic!
+  try {
+    const newPost = postDb.update(req.params.id, req.body);
+    res.status(200).json(newPost);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
+async function validatePostId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  try {
+    const isValidId = await postDb.getById(id);
+    if (isValidId) {
+      req.post = isValidId;
+      next();
+    } else {
+      res.status(404).json({ message: `Not found` });
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = router;
